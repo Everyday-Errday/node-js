88730f4173e38a84130d9732f8527d63c0be54aa
euije <exitfill1234@naver.com>
2023-01-07 21:34:03 +0900

    feat: Add test GET

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
 import * as schedule from "node-schedule";
+import * as express from 'express';
 
-const job = schedule.scheduleJob('10 * * * * *', function(fireDate: Date){
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname + '/static'));
+
+app.get('/items', (req, res) => {
+  res.send("hi");
+});
+
+const job = schedule.scheduleJob('10 * * * * *', (fireDate: Date) => {
   console.log('10따리' + fireDate);
 });
 
-const job1 = schedule.scheduleJob('20 * * * * *', function(fireDate: Date){
+const job1 = schedule.scheduleJob('20 * * * * *', (fireDate: Date) => {
   console.log('20따리' + fireDate);
 });
 
-const job2 = schedule.scheduleJob('30 * * * * *', function(fireDate: Date){
+const job2 = schedule.scheduleJob('30 * * * * *', (fireDate: Date) => {
   console.log('30따리' + fireDate);
 });
 
-const job3 = schedule.scheduleJob('40 * * * * *', function(fireDate: Date){
+const job3 = schedule.scheduleJob('40 * * * * *', (fireDate: Date) => {
   console.log('40따리' + fireDate);
 });
 
-const job4 = schedule.scheduleJob('50 * * * * *', function(fireDate: Date){
+const job4 = schedule.scheduleJob('50 * * * * *', (fireDate: Date) => {
   console.log('50따리' + fireDate);
 });
 
-const job5 = schedule.scheduleJob('0 * * * * *', function(fireDate: Date){
+const job5 = schedule.scheduleJob('0 * * * * *', (fireDate: Date) => {
   console.log('50따리' + fireDate);
-});
+}); 
